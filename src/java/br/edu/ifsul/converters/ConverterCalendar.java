23bc8c12c953a7619cd480897e873f363e6bf206
renatoanunciacao <wagnerpunk002@gmail.com>
2018-06-28 00:02:09 -0300

    Projeto de Locação de filmes

---
+++ src/java/br/edu/ifsul/converters/ConverterCalendar.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.converters;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Renato
+ */
+@FacesConverter(value = "converterCalendar")
+public class ConverterCalendar implements Serializable, Converter {
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    @PersistenceContext(unitName = "APLICACAOLOCADORA-WEBPU")
+    private EntityManager em;
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        try {
+            Calendar retorno = Calendar.getInstance();
+            retorno.setTime(sdf.parse(string));
+            return retorno;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        if (o == null) {
+            return null;
+        }
+        Calendar obj = (Calendar) o;
+        return sdf.format(obj.getTime());
+    }
+
+}
